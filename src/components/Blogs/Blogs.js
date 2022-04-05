fdfc09403eef1c9d96498429a14016af71247c50
Monirul-98 <monirulsifat@gmail.com>
2022-04-05 11:21:29 +0600

    Question answer added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,43 @@ import React from "react";
 const Blogs = () => {
   return (
     <div>
-      <h2>This is blogs</h2>
+      <h2 className="text-3xl text-violet-900 font-bold my-7">
+        What is the purpose of context API?
+      </h2>
+      <p className="text-lg">
+        <b>Answer: </b>Context API is a component structure which is provided by
+        the REACT framework. It solved the problem of porps drilling. Context
+        API helps us to share some specific data across the children components.
+        When we have to pass a prop through a component so it reaches another
+        component down the tree then we can use context API. Sometimes users
+        need to change the theme of their app. Then we simply connect the data
+        in the context Provider to the state of the parent element and then
+        change this parent’s state using functions in props. A better way is to
+        have the function that will change the context value passed down in the
+        context itself.
+      </p>
+      <h2 className="text-3xl text-violet-900 font-bold my-7">
+        What is semantic tag?
+      </h2>
+      <p className="text-lg">
+        <b>Answer: </b>Semantic HTML add meaning to the web page rather than
+        representation. Like I used a paragraph p tag that indicates that this
+        answer is a paragraph. People know what paragraph is and browsers know
+        the way of displaying them so that's why it is both semantic and
+        presentational. On the other hand I used bold b tag it is not semantic
+        cause they are only defining how the text will look. Semantic HTML tags
+        gives information about the contents of those tags that goes beyond just
+        how they look on a page. Like for coding languages there is a code tag
+        using this tag browser recognize that it is some kind of coding
+        language. Some examples of Semantic tags are :
+        <ul className="font-semibold list-inside list-disc">
+          <li>blockquote</li>
+          <li>cite</li>
+          <li>h1,h2,h3,h4,h5 and h6.</li>
+          <li>address</li>
+          <li>ul</li>
+        </ul>
+      </p>
     </div>
   );
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -72,35 +72,36 @@ const Dashboard = () => {
         </h2>
         <AreaChart
           width={600}
-          height={250}
+          height={300}
           data={data}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
           <defs>
             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              <stop offset="5%" stopColor="#721B4F" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#721B4F" stopOpacity={0} />
             </linearGradient>
             <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              <stop offset="5%" stopColor="#1B4F72 " stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#1B4F72 " stopOpacity={0} />
             </linearGradient>
           </defs>
           <XAxis dataKey="month" />
           <YAxis />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
+          <Legend></Legend>
           <Area
             type="monotone"
             dataKey="investment"
-            stroke="#8884d8"
+            stroke="#721B4F"
             fillOpacity={1}
             fill="url(#colorUv)"
           />
           <Area
             type="monotone"
             dataKey="revenue"
-            stroke="#82ca9d"
+            stroke="#1B4F72 "
             fillOpacity={1}
             fill="url(#colorPv)"
           />
